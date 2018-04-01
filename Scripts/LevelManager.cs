a55880ebfd798c4187194b4295453e66e255a375
mattcdrake <drake.mattc@gmail.com>
2018-04-01 18:43:11 -0400

    Added files to git

---
+++ Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string nextLevel)
+    {
+        Debug.Log("Level load requested for " + nextLevel);
+        Application.LoadLevel(nextLevel);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+}
