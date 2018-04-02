3bffea023b8679dc02f379409d063b6322ae7624
mattcdrake <drake.mattc@gmail.com>
2018-04-02 15:35:07 -0400

    Added some scripts and new assets

---
+++ Scripts/LossCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LossCollider : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Application.LoadLevel("StartMenu");
+        Debug.Log("Collision");
+    }
+}
